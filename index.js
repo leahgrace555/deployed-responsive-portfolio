8dca37b377a1888743d60b90789b4f0d2af30ffa
Leah <leah.grace555@gmail.com>
2020-06-08 17:34:44 -0400

    attempt to fix server

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('./Public'))
+app.use(express.static('/Public'))
 
-// app.get('/', function (req, res) {res.send('Hello World!')
-// })
+app.get('/', function (req, res) {res.send('Hello World!')
+})
 
 app.listen(3000)
 
