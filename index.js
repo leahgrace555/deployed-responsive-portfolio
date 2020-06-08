bbf4bd61ea77c4e57cd407a291de4aba5049ce56
Leah <leah.grace555@gmail.com>
2020-06-08 18:20:52 -0400

    testing

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('/Public'))
+app.use(express.static('./Public'))
 
-app.get('/', function (req, res) {res.send('Hello World!')
+app.get('/', function (req, res) {res.send('index')
 })
-
+console.log('just whatever');
 app.listen(3000)
 
 
